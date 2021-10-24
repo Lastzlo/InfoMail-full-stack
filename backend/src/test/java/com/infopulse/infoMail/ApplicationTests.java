6e85421c5615c0f16286e9cb41301479ebc7efea
Lastzlo <z.last.zlo@gmail.com>
2021-10-25 01:09:06 +0300

    init project

---
+++ backend/src/test/java/com/infopulse/infoMail/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.infopulse.infoMail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+    }
+}
